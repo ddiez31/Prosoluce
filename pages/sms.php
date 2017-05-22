1d717c9d33eb53673d27f61520a57766b15a683f
PatTaylor <patrick.taylor31@gmail.com>
2017-05-22 14:19:42 +0200

    Dur dur le début ...

---
+++ pages/sms.php
@@ -0,0 +1,19 @@
+ form id="general">
+        <input id="Form1" type="text" name="champ1" value="Name">
+        <input type="text" name="utilisateur" value="Utilisateur">
+        <input type="text" name="mdp" value="Mot de passe">
+        <input type="text" name="idroute" value="Route">
+        <input type="text" name="senderid" value="Expéditeur SMS">
+        <input type="text" name="callerid" value="Numéro Appellant">
+        <input type="text" name="templateid" value="templateid">
+        <input type="text" name="message" value="Message">
+        <input type="text" name="senddate" value="Date d'envoi">
+        <input type="text" name="grpid" value="Identité du groupe">
+        <input type="text" name="dest" value="Destination">
+        <input type="text" name="globaloperator" value="Opérateur Global">
+        <input type="text" name="champfiltre" value="Champ du groupe d'envoi">
+<input type="text" name="opfiltre" value="Operateur de filtre">
+<input type="text" name="valfiltre" value="Indicatif">
+<input type="text" name="indicatif" value="Veleur du filtre">
+<input type="text" name="numéro" value="Numéro de tél">
+    </form>

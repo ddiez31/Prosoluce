9c481ac2649fcb04927f37e3950921dd0fa4a26d
raphael <moudens.raphael@orange.fr>
2017-05-26 09:47:04 +0200

    recup base

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ include ('pages/header.php');
 
 
 
-include ('pages/categories.php');
+include('ecampaign_demo_laurent.php');
+
+$test = new ecampaign("dm-simplon","SimplonERN");
+$test->sendSMS(1,array(array('+33','0687560651')),'test');
+
 
 
 

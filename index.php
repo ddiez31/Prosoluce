b5c9dc9089446b14654e6b76f8933b0dcf8695c2
David <david.diez31@free.fr>
2017-05-26 09:38:49 +0200

    classes sms mail ok

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 include ('pages/header.php');
 
 
+// include ('pages/categories.php');
 
-include ('pages/categories.php');
+// include ('classes/ecampaign_class_sms.php');
+// $testSms = new ecampaign('dm-simplon', 'SimplonERN');
+// $testSms->sendSMS(1, array(array('+33', '0695501119')), 'C\'est pas bien d\'être jaloux!!');
 
+
+// include ('classes/ecampaign_class_mail.php');
+// $testMail = new ecampaign('dm-simplon', 'SimplonERN');
+// $testMail->sendMail(9, array(array('david.diez31@free.fr')), 'test sujet', 'test message');
+
+
+// include ('classes/ecampaign_class_mail.php');
+// $testMail = new ecampaign('dm-simplon', 'SimplonERN');
+// $testMail->sendMail(9, array(array('david.diez31@free.fr')), 'test sujet', 'test message');
 
 
 include ('pages/footer.php');

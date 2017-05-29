6870dd04028de48ad4c8fed382e12ea9478f863e
David <david.diez31@free.fr>
2017-05-29 23:38:45 +0200

    classe appel + modif routage

--- index.php
+++ index.php
@@ -19,22 +19,29 @@ getPart('footer');
 
 
 // test tableau descrptif Julien de là!!!
-// include ('pages/tables/sms_default.php');
-// include ('pages/tables/sms_synchrone.php');
-// include ('pages/tables/sms_asynchrone.php');
-// include ('pages/tables/create_groupe.php');
-// include ('pages/tables/import_contact_xml.php');
-// include ('pages/tables/create_modif_model.php');
-// include ('pages/tables/recorded_model_voice.php');
-// include ('pages/tables/download_recorded_model_voice.php');
-// include ('pages/tables/consult_cred_disp.php');
-// include ('pages/tables/obtain_list_receiver_blacklist.php');
-// include ('pages/tables/supp_member_blacklist.php');
-// include ('pages/tables/obtain_responses.php');
-// include ('pages/tables/call_parameters.php');
-// include ('pages/tables/parameters_get.php');
-// include ('pages/tables/info_return.php');
-// include ('pages/code_errors/code_communs.php');
+// include ('pages/tables/table_send_sms.php');
+// include ('pages/tables/table_descr_gest_grp_envoi.php');
+// include ('pages/tables/table_gest_grp_envois.php');
+// include ('pages/tables/table_gest_cred.php');
+// include ('pages/tables/table_gest_answer_and_blacklist.php');
+// include ('pages/tables/table_suivi_envois.php.php');
+// include ('pages/tables/table_info_number_phone.php.php');
+// include ('pages/tables/table_sms_default.php');
+// include ('pages/tables/table_sms_synchrone.php');
+// include ('pages/tables/table_sms_asynchrone.php');
+// include ('pages/tables/table_create_groupe.php');
+// include ('pages/tables/table_import_contact_xml.php');
+// include ('pages/tables/table_create_modif_model.php');
+// include ('pages/tables/table_recorded_model_voice.php');
+// include ('pages/tables/table_download_recorded_model_voice.php');
+// include ('pages/tables/table_consult_cred_disp.php');
+// include ('pages/tables/table_obtain_list_receiver_blacklist.php');
+// include ('pages/tables/table_supp_member_blacklist.php');
+// include ('pages/tables/table_obtain_responses.php');
+// include ('pages/tables/table_call_parameters.php');
+// include ('pages/tables/table_parameters_get.php');
+// include ('pages/tables/table_info_return.php');
+// include ('pages/code_errors/codes_communs.php');
 // include ('pages/code_errors/codes_méthodes_send.php');
 // include ('pages/code_errors/codes_relat_group_send.php');
 // include ('pages/code_errors/codes_relat_gest_model.php');
@@ -42,7 +49,7 @@ getPart('footer');
 // include ('pages/code_errors/codes_relat_suivi_envois.php');
 // include ('pages/code_errors/codes_status.php');
 // include ('pages/code_errors/codes_errors.php');
-// include ('pages/code_errors/etat_remise.php');
+// include ('pages/code_errors/codes_etat_remise.php');
 // test tableau descriptif Julien À de là!!!
 
 

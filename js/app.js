7059bb405c3d853256a63b95c086d8ea3773b912
PatTaylor <patrick.taylor31@gmail.com>
2017-05-30 15:00:57 +0200

    Le fade In maaaaarrrchheeeee !!!!!!!!

--- js/app.js
+++ js/app.js
@@ -6,5 +6,18 @@ $(document).ready(function() {
     $('.ui.dropdown').dropdown();
 
 
+    console.log($(location).attr('href'));
+
+    if ($(location).attr('href') !== "http://localhost:8000/") {
+        $(".ui.menu.ui.dropdown.menu>.item").click(function() {
+            $(".container").fadeIn();
+        });
+    } else {
+
+        $(".container").hide();
+    };
+
+
+
 
 });
--- lib/functions.php
+++ lib/functions.php
@@ -13,8 +13,58 @@ function getContent() {
 		include __DIR__.'/../pages/compose/compose_voice.php';
 	} if(isset($_GET['compose_mail'])) {
 		include __DIR__.'/../pages/compose/compose_mail.php';
-	} if(isset($_GET['admin'])) {
-		include __DIR__.'/../public/admin.php';
+	} if(isset($_GET['groupes_add_fields_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_add_fields_groups.php';
+	} if(isset($_GET['groupes_add_user'])) {
+		include __DIR__.'/../pages/groupes/groupes_add_user.php';
+	} if(isset($_GET['groupes_change_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_change_groups.php';
+	} if(isset($_GET['groupes_change_user'])) {
+		include __DIR__.'/../pages/groupes/groupes_change_user.php';
+	} if(isset($_GET['groupes_create_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_create_groups.php';
+	} if(isset($_GET['groupes_delete_contains_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_delete_contains_groups.php';
+	} if(isset($_GET['groupes_delete_fields_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_delete_fields_groups.php';
+	} if(isset($_GET['groupes_delete_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_delete_groups.php';
+	} if(isset($_GET['groupes_delete_user'])) {
+		include __DIR__.'/../pages/groupes/groupes_delete_user.php';
+	} if(isset($_GET['groupes_import_contacts_XML'])) {
+		include __DIR__.'/../pages/groupes/groupes_import_contacts_XML.php';
+	} if(isset($_GET['groupes_info_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_info_groups.php';
+	} if(isset($_GET['groupes_list_groups'])) {
+		include __DIR__.'/../pages/groupes/groupes_list_groups.php';
+	} if(isset($_GET['groupes_search_user'])) {
+		include __DIR__.'/../pages/groupes/groupes_search_user.php';
+	} if(isset($_GET['templates_add_rec'])) {
+		include __DIR__.'/../pages/templates/templates_add_rec.php';
+	} if(isset($_GET['templates_create_change'])) {
+		include __DIR__.'/../pages/templates/templates_create_change.php';
+	} if(isset($_GET['templates_delete'])) {
+		include __DIR__.'/../pages/templates/templates_delete.php';
+	} if(isset($_GET['templates_download_rec'])) {
+		include __DIR__.'/../pages/templates/templates_download_rec.php';
+	} if(isset($_GET['templates_list'])) {
+		include __DIR__.'/../pages/templates/templates_list.php';
+	} if(isset($_GET['templates_news'])) {
+		include __DIR__.'/../pages/templates/templates_news.php';
+	} if(isset($_GET['credits_management_credits'])) {
+		include __DIR__.'/../pages/credits/credits_management_credits.php';
+	} if(isset($_GET['feedback_answers_blacklist'])) {
+		include __DIR__.'/../pages/feedback/feedback_answers_blacklist.php';
+	} if(isset($_GET['feedback_get_answers'])) {
+		include __DIR__.'/../pages/feedback/feedback_get_answers.php';
+	} if(isset($_GET['feedback_remove_member_blacklist'])) {
+		include __DIR__.'/../pages/feedback/feedback_remove_member_blacklist.php';
+	} if(isset($_GET['tracking_get'])) {
+		include __DIR__.'/../pages/tracking/tracking_get.php';
+	} if(isset($_GET['tracking_recovery_mail'])) {
+		include __DIR__.'/../pages/tracking/tracking_recovery_mail.php';
+	} if(isset($_GET['info_info_hlr'])) {
+		include __DIR__.'/../pages/info/info_info_hlr.php';
 	}
 }
 
@@ -32,4 +82,9 @@ function admin() {
 	echo 'function admin ok';
 }
 
+ 
+
+
 ?>
+
+

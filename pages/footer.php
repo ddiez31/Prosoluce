f5aea51c55521a4e3848c85b87d0b32b9aba3094
PatTaylor <patrick.taylor31@gmail.com>
2017-05-30 09:18:33 +0200

    Commit du mardi matin

--- pages/footer.php
+++ pages/footer.php
@@ -1,11 +1,19 @@
+	</div>
+	
+	
 	<footer id="footer">
 	<div id="box-footer">
-	<a href="https://www.prosoluce.fr/mentions-legales.html">PROSOLUCE 2007-2017</a><br><br>
-		<a href="https://www.prosoluce.fr/mentions-legales.html">Mentions légales</a><br>
-		<a href="https://www.prosoluce.fr/contacter-prosoluce.html">Contact</a><br>
+		<a href="https://www.prosoluce.fr/mentions-legales.html">Mentions légales&nbsp; - &nbsp;</a>
+		<a id="footersoluce" href="https://www.prosoluce.fr/mentions-legales.html">PROSOLUCE 2007/2017&nbsp; </a>
+		<a href="https://www.prosoluce.fr/contacter-prosoluce.html">- &nbsp;Contact</a>
 	</div>
 
 </footer>
+
+
+
+
+
 	<a id="haut" href="https://www.prosoluce.fr/#haut-page" style="opacity: 1;"></a>
 		<script type="text/javascript" src="./Prosoluce, développeur de contacts_files/teacookie.min.js"></script>
 	<script type="text/javascript" src="./Prosoluce, développeur de contacts_files/cycle2.min.js"></script>
@@ -22,7 +30,7 @@
 
 </body>
 
-<script src="../../js/jquery-3.2.1.min.js"></script>
-<script src="../../js/semantic-2.2.10.min.js"></script>
-<script src="../../js/app.js"></script>
+<script src="http://localhost:8000/js/jquery-3.2.1.min.js"></script>
+<script src="http://localhost:8000/js/semantic-2.2.10.min.js"></script>
+<script src="http://localhost:8000/js/app.js"></script>
 </html>
--- pages/header.php
+++ pages/header.php
@@ -1,27 +1,27 @@
-<!DOCTYPE html>
-<!-- saved from url=(0025)https://www.prosoluce.fr/ -->
-<html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<!--<base href="/">--><base href=".">
-<title>Prosoluce, développeur de contacts</title>
-<meta name="description" content="Solutions de communication multicanal, logiciels de prospection, applications téléphoniques, intégration web, mobile et services d&#39;infrastructure.">
-<meta name="keywords" content="prosoluce, communication, multicanal, omnicanal, infrastructure, reseau, web, hebergement, developpement, integration, solutions, applications, mobile, marketing, telemarketing, ndd, nom de domaine"> 
-<meta name="generator" content="PROSOLUCE 2007-2017">
+$(document).ready(function() {
+    // navigation des onglets
+    $('#tabnavigation .item').tab();
+
+    $('.ui.dropdown')
+        .dropdown();
 
 
-<meta name="viewport" content="width=device-width">
-<!--<base href="http://www.prosoluce.fr/">-->
-<link rel="icon" type="image/png" href="https://www.prosoluce.fr/uploads/design/imgs/favicon.png">
 
-<script src="../../js/jquery-3.2.1.min.js"></script>
-<script src="../../js/semantic-2.2.10.min.js"></script>
-<link rel="stylesheet" type="text/css" href="../../style/semantic.min.css">
-<link rel="stylesheet" type="text/css" href="../../style/style.css">
+    // $(".container").css({ display: "none" });
+    // $('.ui.menu .ui.dropdown .menu>.item').click(function() {
+    //     $(".container").css({ display: "block" });
+    // });
+
+});ript src="http://localhost:8000/js/jquery-3.2.1.min.js"></script>
+<script src="http://localhost:8000/js/semantic-2.2.10.min.js"></script>
+<link rel="stylesheet" type="text/css" href="http://localhost:8000/style/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="http://localhost:8000/style/style.css">
 
 
 </head><body cz-shortcut-listen="true">
 <div id="haut-page" class="pousse-footer">
   <header class="ui fixed menu navbar">
-   <a href="https://www.prosoluce.fr/"><img id="logo" src="assets/logo-prosoluce.png" alt="Logo PROSOLUCE"></a>
+   <a href="https://www.prosoluce.fr/"><img id="logo" src="http://localhost:8000/assets/logo-prosoluce.png" alt="Logo PROSOLUCE"></a>
    <div id="burger"></div>
  </header>
 
@@ -34,53 +34,60 @@
     </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Structure de groupes<div class="menu">
-      <div class="item">Recherche de membres</div>
-      <div class="item">Ajout d'un membre</div>
-      <div class="item">Modification d'un membre</div>
-      <div class="item">Suppression d'un membre</div>
+      <a class="item" href="/pages/groupes/groupes_search_user.php">Recherche de membres</a>
+      <a class="item" href="/pages/groupes/groupes_add_user.php">Ajout d'un membre</a>
+      <a class="item" href="/pages/groupes/groupes_change_user.php">Modification d'un membre</a>
+      <a class="item" href="/pages/groupes/groupes_delete_user.php">Suppression d'un membre</a>
     </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Contenu de groupes<div class="menu">
-      <div class="item">Liste des groupes existants</div>
-      <div class="item">Informations sur un groupe</div>
-      <div class="item">Création d'un groupe</div>
-      <div class="item">Modification d'un groupe</div>
-      <div class="item">Supprime le contenu d'un groupe d'envoi</div>
-      <div class="item">Import de contacts depuis un fichier XML</div>
-      <div class="item">Ajouter des champs dans un groupe</div>
-      <div class="item">Supprimer des champs dans un groupe</div>
+      <a class="item" href="/pages/groupes/groupes_list_groups.php">Liste des groupes existants</a>
+      <a class="item" href="/pages/groupes/groupes_info_groups.php">Informations sur un groupe</a>
+      <a class="item" href="/pages/groupes/groupes_create_groups.php">Création d'un groupe</a>
+      <a class="item" href="/pages/groupes/groupes_change_groups.php">Modification d'un groupe</a>
+      <a class="item" href="/pages/groupes/groupes_delete_contains_groups.php">Supprime le contenu d'un groupe d'envoi</a>
+      <a class="item" href="/pages/groupes/groupes_import_contacts_XML.php">Import de contacts depuis un fichier XML</a>
+      <a class="item" href="/pages/groupes/groupes_add_fields_groups.php">Ajouter des champs dans un groupe</a>
+      <a class="item" href="/pages/groupes/groupes_delete_fields_groups.php">Supprimer des champs dans un groupe</a>
     </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Gestion des modèles<div class="menu">
-      <div class="item">Création ou modification d'un modèle</div>
-      <div class="item">Suppression d'un modèle</div>
-      <div class="item">Informations sur un modèle</div>
-      <div class="item">Liste de tous les modèles existants</div>
-      <div class="item">Ajoute un enregistrement au sein d'un modèle "voix"</div>
-      <div class="item">Télécharge un enregistrement présent dans un modèle voix</div>
-      <div class="item">Ajouter des champs dans un groupe</div>
-      <div class="item">Supprimer des champs dans un groupe</div>
+      <a class="item" href="/pages/templates/templates_create_change.php">Création ou modification d'un modèle</a>
+      <a class="item" href="/pages/templates/templates_delete.php">Suppression d'un modèle</a>
+      <a class="item" href="/pages/templates/templates_news.php">Informations sur un modèle</a>
+      <a class="item" href="/pages/templates/templates_list.php">Liste de tous les modèles existants</a>
+      <a class="item" href="/pages/templates/templates_add_rec.php">Ajoute un enregistrement au sein d'un modèle "voix"</a>
+      <a class="item" href="pages/templates/templates_download_rec.php">Télécharge un enregistrement présent dans un modèle voix</a>
     </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Gestion des crédits<div class="menu">
-      <div class="item">Consultation des crédits disponibles</div>
+      <a class="item" href="pages/credits/credits_management_credits.php">Consultation des crédits disponibles</a>
       </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Gestion des réponses et blacklists<div class="menu">
-      <div class="item">Obtention de la liste des destinataires blacklistés</div>
-      <div class="item">Suppression d'un membre au sein d'une blacklist</div>
-      <div class="item">Obtention des réponses</div>
-    </div>
+      <a class="item" href="pages/feedback/feedback_answers_blacklist.php">Obtention de la liste des destinataires blacklistés</a>
+      <a class="item" href="pages/feedback/feedback_remove_member_blacklist.php">Suppression d'un membre au sein d'une blacklist</a>
+      <a class="item" href="pages/feedback/feedback_get_answers.php">Obtention des réponses</a>
+      </div>    
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Suivi des renvois<div class="menu">
-      <div class="item">Récupération de la liste des messages envoyés avec suivi</div>
-      <div class="item">Email : récupération du suivi des actions</div>
+      <a class="item" href="/pages/tracking/tracking_get.php">Récupération de la liste des messages envoyés avec suivi</a>
+      <a class="item" href="/pages/tracking/tracking_recovery_mail.php">Email : récupération du suivi des actions</a>
     </div>
   </div>
   <div class="ui left pointing dropdown link item"><i class="dropdown icon"></i>Obtention d'informations sur les numéros de tél<div class="menu">
-      <div class="item">Obtention d'informations HLR</div>
+      <a class="item" href="pages/info/info_info_hlr.php">Obtention d'informations HLR</a>
     </div>
   </div>
 </div>
+<?php
 
+if ((!isset($_GET=='index.php'){
+
+}
+
+
+?>
+
+<div class="container">
 

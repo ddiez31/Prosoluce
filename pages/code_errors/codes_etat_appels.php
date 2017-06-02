03511753c8d8e310b5b0333dfc1f10eb3aff86ce
David <david.diez31@free.fr>
2017-06-02 16:41:11 +0200

    dossier code errors

---
+++ pages/code_errors/codes_etat_appels.php
@@ -0,0 +1,86 @@
+<table class="ui fixed line celled table">
+	<thead>
+		<tr><th>Code</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>Message envoyé à l'opérateur</td>
+		</tr>
+		<tr>
+			<td>2 ou 3</td>
+			<td>Message en attente de remise sur le réseau</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>Message remis au destinataire</td>
+		</tr>
+		<tr>
+			<td>7</td>
+			<td>Opérateur indisponible</td>
+		</tr>
+		<tr>
+			<td>8, 9 ou 10</td>
+			<td>Remise impossible</td>
+		</tr>
+		<tr>
+			<td>11</td>
+			<td>Appel décroché</td>
+		</tr>
+		<tr>
+			<td>12</td>
+			<td>Introduction : pas de choix</td>
+		</tr>
+		<tr>
+			<td>13</td>
+			<td>Répondeur : enregistré</td>
+		</tr>
+		<tr>
+			<td>14</td>
+			<td>Début de l'écoute du message</td>
+		</tr>
+		<tr>
+			<td>15</td>
+			<td>Message supprimé</td>
+		</tr>
+		<tr>
+			<td>16</td>
+			<td>Appels</td>
+			<td>Message écouté</td>
+		</tr>
+		<tr>
+			<td>17</td>
+			<td>Pas de réponse à la question</td>
+		</tr>
+		<tr>
+			<td>2X</td>
+			<td>Choix n°X (exemples : « 20 » = choix 0 / « 23 » = choix 3...)</td>
+		</tr>
+		<tr>
+			<td>40 ou 41</td>
+			<td>Ligne hors service</td>
+		</tr>
+		<tr>
+			<td>42</td>
+			<td>Appel non décroché</td>
+		</tr>
+		<tr>
+			<td>43</td>
+			<td>Ligne occupée</td>
+		</tr>
+		<tr>
+			<td>44</td>
+			<td>Réseau de destination saturé</td>
+		</tr>
+		<tr>
+			<td>45</td>
+			<td>Répondeur détecté, appel raccroché</td>
+		</tr>
+		<tr>
+			<td>46</td>
+			<td>Humain détecté, appel raccroché</td>
+		</tr>
+	</tbody>
+</table>
---
+++ pages/code_errors/codes_etat_fax.php
@@ -0,0 +1,46 @@
+<table class="ui fixed line celled table">
+	<thead>
+		<tr><th>Code</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>Message envoyé à l'opérateur</td>
+		</tr>
+		<tr>
+			<td>2 ou 3</td>
+			<td>Message en attente de remise sur le réseau</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>Message remis au destinataire</td>
+		</tr>
+		<tr>
+			<td>7</td>
+			<td>Opérateur indisponible</td>
+		</tr>
+		<tr>
+			<td>8, 9 ou 10</td>
+			<td>Remise impossible</td>
+		</tr>
+		<tr>
+			<td>60 et 61</td>
+			<td>Remise impossible</td>
+		</tr>
+		<tr>
+			<td>62</td>
+			<td>Ligne occupée</td>
+		</tr>
+		<tr>
+			<td>63</td>
+			<td>Non réponse</td>
+		</tr>
+		<tr>
+			<td>64</td>
+			<td>Remise partielle ou impossible</td>
+		</tr>
+
+	</tbody>
+</table>
